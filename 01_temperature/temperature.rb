31a2bd9d54b303d7be475da0f7221201579c11f0
Rui Dong <rui.dong.jh@gmail.com>
2016-08-16 13:29:01 -0400

    committing again with finished code

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,10 +1,12 @@
 # Method to convert F to C
 def ftoc(degrees)
-  degrees = (32 - degrees) * 5.0/9
+  degrees = (degrees - 32) * 5.0/9
+  degrees.to_i
 end
 
 # Method to convert C to F
 
 def ctof(degrees)
-  degrees = 32 - degrees* 9/5.0
+  degrees = 32 + degrees* 9/5.0
+  degrees.to_i
 end
